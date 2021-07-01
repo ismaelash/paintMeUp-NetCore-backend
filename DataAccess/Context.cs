5971537ebf56c1010704119043e0106a0b3b2243
Ismael Nascimento 'Ash <contato@ismaelnascimento.com>
2021-07-01 00:14:34 -0300

    Initial commit

---
+++ DataAccess/Context.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Mappings;
+using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+               @"Server=serverTemplate;Initial Catalog=Template;Persist Security Info=False;User ID=template;Password=template;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new TemplateMappings());
+
+        }
+
+        public DbSet<Template> Template { get; set; }
+
+    }
+}
